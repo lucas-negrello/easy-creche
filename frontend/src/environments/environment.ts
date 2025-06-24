1a8b48f60800b35c916ff3b56c48365850a84ae9
lucas <lucassnegrello@gmail.com>
2025-06-24 03:09:20 +0000

    adjust frontend environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   currentEnvironment: 'production',
   baseUrl: 'http://35.198.24.52:8080',
-  apiUrl: 'http://35.198.24.52:8080',
+  apiUrl: 'http://35.198.24.52:8080/api',
   pixKey: '+5554999314787'
 };
