a21c720be7c1b0f75495a15f0ff789bc041a30b0
lucas <lucassnegrello@gmail.com>
2025-06-25 22:31:35 +0000

    updated frontend prd env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   currentEnvironment: 'production',
-  baseUrl: 'http://easycreche-api.lsnegrello.com.br',
-  apiUrl: 'http://easycreche-api.lsnegrello.com.br/api',
+  baseUrl: 'http://easycreche_api.lsnegrello.com.br',
+  apiUrl: 'http://easycreche_api.lsnegrello.com.br/api',
   pixKey: '+5554999314787'
 };
