32cfe015af55d99616cae5b2f15a3627aca5cd87
Lucas Negrello <127449526+lucas-negrello@users.noreply.github.com>
2025-06-25 19:27:23 -0300

    Update Cors.php

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -22,6 +22,8 @@ class Cors
         }
 
         $response->headers->set('Access-Control-Allow-Origin', 'http://35.198.24.52');
+        $response->headers->set('Access-Control-Allow-Origin', 'http://easycreche.lsnegrello.com.br');
+        $response->headers->set('Access-Control-Allow-Origin', 'https://easycreche.lsnegrello.com.br');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         // Se precisar, habilite credenciais:
